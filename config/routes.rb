403a2f4b354297cbbe4e7cad14545539cd264e34
Josue Gutierrez <joxu.mac@gmail.com>
2018-05-16 12:21:42 +0200

    - New Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,31 @@
 Rails.application.routes.draw do
+
+  #FRIEND REQUEST
   resources :friendship_requests
+  
+  #USERS
   get "logout" => "session#destroy", :as => "logout"
   get "login" => "session#new", :as => "login"
   post "login" => "session#create"
   get "signup" => "users#new", :as => "signup"
   resources :users
   resources :session
+  
+  #NOTES
   resources :notes
   get "allnotes" => "notes#allnotes"
+  
+  #USER NOTES
   resources :user_notes
   root :to => "session#new"
+  
+  #COLLECTIONS
+  resources :collections
+  
+  #FRIENDS
+  resources :friends
+  
+  
   
   #get 'session/new'
 
@@ -17,12 +33,8 @@ Rails.application.routes.draw do
 
   #get 'session/destroy'
 
-  #resources :friends
   #resources :collection_users
   #resources :collection_notes
-  #resources :collections
-  #resources :user_notes
-  #resources :notes
-  #resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
