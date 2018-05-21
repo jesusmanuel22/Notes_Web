e595ed427491001887cf1e15e114715324b104b1
Josue Gutierrez <joxu.mac@gmail.com>
2018-05-21 15:52:46 +0200

    New Note View

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -65,7 +65,7 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.html { redirect_to '/notes'}#, notice: 'Note was successfully created.' }
         format.json { render :show, status: :created, location: @note }
         @user_note = UserNote.new
         @user=User.find_by name: session[:user]
