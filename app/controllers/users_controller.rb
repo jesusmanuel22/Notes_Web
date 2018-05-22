950a6969b2de9a254f52d22079abad54adb87b78
Josue Gutierrez <joxu.mac@gmail.com>
2018-05-22 15:15:51 +0200

    Profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,15 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def profile
+  
+	@user = User.find_by name: session[:user]
+
+    @numnotes = (ActiveRecord::Base.connection.execute("SELECT 'notes'.* FROM 'notes', 'user_notes' WHERE 'notes'.'id' == 'user_notes'.id_note AND 'user_notes'.id_user == #{@user.id}")).count
+
+	@numfriends = Friend.where('id_user1 LIKE ? OR id_user2 LIKE ?',"#{@user.id}","#{@user.id}").count
+  end
+  
   # POST /users
   # POST /users.json
   def create
